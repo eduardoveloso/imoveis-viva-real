d839708903281f1b4e497260a9072fc8711de7cc
eduardoveloso <eveloso92@gmail.com>
2022-11-02 22:44:54 -0300

    modify main create df

--- main/imoveis.py
+++ main/imoveis.py
@@ -31,45 +31,55 @@ i = 0
 # %%
 ret = requests.get(url.format(estado, cidade, i))
 soup = bs(ret.text)
-houses =  soup.find_all('a', {'class': 'property-card__content-link js-card-title'})
-qty_houses = int(soup.find('strong', {'class': 'results-summary__count'}).text.replace('.', ''))
+houses = soup.find_all(
+    'a', {'class': 'property-card__content-link js-card-title'})
+qty_houses = int(
+    soup.find('strong', {'class': 'results-summary__count'}).text.replace('.', ''))
 pages = ceil(qty_houses / len(houses))
 
 while qty_houses <= pages:
     i += 1
     print(f"valor i: {i} \t\t qtd_imoveis: {df.shape[0]}")
-    
+
     for house in houses:
         try:
-            descricao = house.find('span', {'class': 'property-card__title'}).text.strip()
+            descricao = house.find(
+                'span', {'class': 'property-card__title'}).text.strip()
         except:
             descricao = None
         try:
-            endereco = house.find('span', {'class': 'property-card__address'}).text.strip()
+            endereco = house.find(
+                'span', {'class': 'property-card__address'}).text.strip()
         except:
             endereco = None
         try:
-            area = house.find('span', {'class': 'js-property-card-detail-area'}).text.strip()
+            area = house.find(
+                'span', {'class': 'js-property-card-detail-area'}).text.strip()
         except:
             area = None
         try:
-            quartos = house.find('li', {'class': 'property-card__detail-room'}).span.text.strip()
+            quartos = house.find(
+                'li', {'class': 'property-card__detail-room'}).span.text.strip()
         except:
             quartos = None
         try:
-            wc = house.find('li', {'class': 'property-card__detail-bathroom'}).span.text.strip()
+            wc = house.find(
+                'li', {'class': 'property-card__detail-bathroom'}).span.text.strip()
         except:
             wc = None
         try:
-            vagas = house.find('li', {'class': 'property-card__detail-garage'}).span.text.strip()
+            vagas = house.find(
+                'li', {'class': 'property-card__detail-garage'}).span.text.strip()
         except:
             vagas = None
         try:
-            valor = house.find('div', {'class': 'property-card__price'}).p.text.strip()
+            valor = house.find(
+                'div', {'class': 'property-card__price'}).p.text.strip()
         except:
             valor = None
         try:
-            condominio = house.find('strong', {'class': 'js-condo-price'}).text.strip()
+            condominio = house.find(
+                'strong', {'class': 'js-condo-price'}).text.strip()
         except:
             condominio = None
         try:
