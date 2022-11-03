c4b44061244f2afdc43f02e639b61f1d840cad43
eduardoveloso <eveloso92@gmail.com>
2022-11-02 22:42:56 -0300

    create dataframe

--- main/imoveis.py
+++ main/imoveis.py
@@ -11,19 +11,87 @@ cidade = cidade.replace(' ', '-').lower()
 
 url = 'https://www.vivareal.com.br/venda/{}/{}/apartamento_residencial/?pagina={}'
 
+
 # %%
-i = 1
+df = pd.DataFrame(
+    columns=[
+        'descricao',
+        'endereco',
+        'area',
+        'quartos',
+        'wc',
+        'vagas',
+        'valor',
+        'condominio',
+        'wlink'
+    ]
+)
+i = 0
+
+# %%
 ret = requests.get(url.format(estado, cidade, i))
 soup = bs(ret.text)
-#soup
-
-# %%
 houses =  soup.find_all('a', {'class': 'property-card__content-link js-card-title'})
-qty_houses = float(soup.find('strong', {'class': 'results-summary__count'}).text.replace('.', ''))
+qty_houses = int(soup.find('strong', {'class': 'results-summary__count'}).text.replace('.', ''))
 pages = ceil(qty_houses / len(houses))
 
-#len(houses)
-#qty_houses
-pages
+while qty_houses <= pages:
+    i += 1
+    print(f"valor i: {i} \t\t qtd_imoveis: {df.shape[0]}")
+    
+    for house in houses:
+        try:
+            descricao = house.find('span', {'class': 'property-card__title'}).text.strip()
+        except:
+            descricao = None
+        try:
+            endereco = house.find('span', {'class': 'property-card__address'}).text.strip()
+        except:
+            endereco = None
+        try:
+            area = house.find('span', {'class': 'js-property-card-detail-area'}).text.strip()
+        except:
+            area = None
+        try:
+            quartos = house.find('li', {'class': 'property-card__detail-room'}).span.text.strip()
+        except:
+            quartos = None
+        try:
+            wc = house.find('li', {'class': 'property-card__detail-bathroom'}).span.text.strip()
+        except:
+            wc = None
+        try:
+            vagas = house.find('li', {'class': 'property-card__detail-garage'}).span.text.strip()
+        except:
+            vagas = None
+        try:
+            valor = house.find('div', {'class': 'property-card__price'}).p.text.strip()
+        except:
+            valor = None
+        try:
+            condominio = house.find('strong', {'class': 'js-condo-price'}).text.strip()
+        except:
+            condominio = None
+        try:
+            wlink = 'https://www.vivareal.com.br' + house['href']
+        except:
+            wlink = None
 
+        df.loc[df.shape[0]] = [
+            descricao,
+            endereco,
+            area,
+            quartos,
+            wc,
+            vagas,
+            valor,
+            condominio,
+            wlink
+        ]
+
+# %%
+df
+
+# %%
+df.to_csv('banco_de_imoveis.csv', sep=';', index=False)
 # %%
